85823c19dfa292a8b18a617d0088569da9ad6230
Pranjal Rai <pranjalrai1@gmail.com>
2024-07-10 01:28:52 +0530

    Todo list with redux-toolkit first draft

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import AddTodo from './components/AddTodo'
+import Todos from './components/Todos'
+
+function App() {
+
+  return (
+    <>
+    To Do List App using Redux Toolkit
+    <AddTodo />
+    <Todos />
+    </>
+  )
+}
+
+export default App
